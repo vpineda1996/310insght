66c32eff132a040b52a95b8a2b24e5b8af1734e3
Hiroki <srf-hiroki727@hotmail.co.jp>
2016-11-14 23:25:50 -0800

    change outdir

---
+++ public/src/rest/public/main.js
@@ -0,0 +1,110 @@
+$(function () {
+    $("#datasetAdd").click(function () {
+        var id = $("#datasetId").val();
+        var zip = $("#datasetZip").prop('files')[0];
+        var data = new FormData();
+        data.append("zip", zip);
+        $.ajax("/dataset/" + id,
+            {
+                type: "PUT",
+                data: data,
+                processData: false
+            }).fail(function (e) {
+            spawnHttpErrorModal(e)
+        });
+    });
+
+    $("#datasetRm").click(function () {
+        var id = $("#datasetId").val();
+        $.ajax("/dataset/" + id, {type: "DELETE"}).fail(function (e) {
+            spawnHttpErrorModal(e)
+        });
+    });
+
+    $("#queryForm").submit(function (e) {
+        e.preventDefault();
+        var query = $("#query").val();
+        try {
+            query = JSON.parse(query);
+
+            $.post("/query", query, function (data) {
+                if (data["render"] === "table") {
+                    generateTable(data["result"]);
+                }
+            }).fail(function (e) {
+                spawnHttpErrorModal(e)
+            });
+        } catch (err) {
+            spawnErrorModal("Query Error", err);
+        }
+    });
+
+    function generateTable(data) {
+        var columns = [];
+        Object.keys(data[0]).forEach(function (title) {
+            columns.push({
+                head: title,
+                cl: "title",
+                html: function (d) {
+                    return d[title]
+                }
+            });
+        });
+        var container = d3.select("#render");
+        container.html("");
+        container.selectAll("*").remove();
+        var table = container.append("table").style("margin", "auto");
+
+        table.append("thead").append("tr")
+            .selectAll("th")
+            .data(columns).enter()
+            .append("th")
+            .attr("class", function (d) {
+                return d["cl"]
+            })
+            .text(function (d) {
+                return d["head"]
+            });
+
+        table.append("tbody")
+            .selectAll("tr")
+            .data(data).enter()
+            .append("tr")
+            .selectAll("td")
+            .data(function (row, i) {
+                return columns.map(function (c) {
+                    // compute cell values for this specific row
+                    var cell = {};
+                    d3.keys(c).forEach(function (k) {
+                        cell[k] = typeof c[k] == "function" ? c[k](row, i) : c[k];
+                    });
+                    return cell;
+                });
+            }).enter()
+            .append("td")
+            .html(function (d) {
+                return d["html"]
+            })
+            .attr("class", function (d) {
+                return d["cl"]
+            });
+    }
+
+    function spawnHttpErrorModal(e) {
+        $("#errorModal .modal-title").html(e.status);
+        $("#errorModal .modal-body p").html(e.statusText + "</br>" + e.responseText);
+        if ($('#errorModal').is(':hidden')) {
+            $("#errorModal").modal('show')
+        }
+    }
+
+    function spawnErrorModal(errorTitle, errorText) {
+        $("#errorModal .modal-title").html(errorTitle);
+        $("#errorModal .modal-body p").html(errorText);
+        if ($('#errorModal').is(':hidden')) {
+            $("#errorModal").modal('show')
+        }
+    }
+});
+
+
