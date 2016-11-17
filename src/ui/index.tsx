6d44d00964b551842296dd561f2ed87be151afd5
Hiroki <srf-hiroki727@hotmail.co.jp>
2016-11-16 22:03:39 -0800

    lol?

--- src/ui/index.tsx
+++ src/ui/index.tsx
@@ -4,6 +4,10 @@ import * as ReactDOM from "react-dom";
 import { Hello } from "./components/Hello";
 
 ReactDOM.render(
-    <Hello compiler="TypeScript" framework="React" />,
+    (<div>
+         <h1>LOL</h1>
+         <h2>yeah you know</h2>
+         <Hello compiler="TypeScript" framework="React" />
+    </div>),
     document.getElementById("example")
 );
