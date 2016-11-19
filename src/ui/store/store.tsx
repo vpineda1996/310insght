81ce0f18ec34abfb77216395ce8abba51f879c46
Hiroki <srf-hiroki727@hotmail.co.jp>
2016-11-18 19:29:02 -0800

    duh

--- src/ui/store/store.tsx
+++ src/ui/store/store.tsx
@@ -39,7 +39,7 @@ export class Store {
         let key = JSON.stringify(query);
         let data: Data = dataContainer[key];
 
-        if (!data || data.value === [] || data.expires_at > new Date().getTime()) return this.load(id, query);
+        if (!data || data.value === [] || data.expires_at < new Date().getTime()) return this.load(id, query);
 
         return new Promise<any[]>((resolve, reject) => resolve(data.value));
     }
@@ -50,19 +50,25 @@ export class Store {
         if (!dataContainer) dataContainer = {};
 
         let data = dataContainer[key];
-        if (!data) data = {
-          id: id,
-          query_key: key,
-          value: [],
-          expires_at: new Date().getTime() + 5 * 60000 // 5 mins
-        };
+        if (!data) {
+            data = {
+                id: id,
+                query_key: key,
+                value: [],
+                expires_at: null
+            };
+        }
+        data.expires_at = new Date().getTime() + 5 * 60000 // 5 mins
 
         return new Promise<any[]>((resolve, reject) => {
             return fetch(id, query).then(res => {
-                console.info(res.result);
+                data.value = res.result;
+                dataContainer[key] = data;
+                this._data[id] = dataContainer;
                 return resolve(res.result);
             }).fail((xhr, status, error) => {
-                console.error(error);
+                dataContainer[key] = data;
+                this._data[id] = dataContainer;
                 return reject(error);
             });
         });
