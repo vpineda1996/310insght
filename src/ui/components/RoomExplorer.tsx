fbac28223562197436454dbf8df1f49ac02f3360
Hiroki <srf-hiroki727@hotmail.co.jp>
2016-11-27 14:35:58 -0800

    move to helper

--- src/ui/components/RoomExplorer.tsx
+++ src/ui/components/RoomExplorer.tsx
@@ -6,6 +6,7 @@ import { RoomFilterProps, SubFilterProps } from './Filter';
 import { Map, MarkerProps } from './Map';
 import { Store, Data } from '../store/store';
 import { ROOMS_COLUMNS } from '../store/constants';
+import { buildRoomQuery, boundQuery } from '../store/RoomStore';
 
 interface RoomExplorerProps {
     dataId: string;
@@ -23,44 +24,6 @@ interface RoomExplorerState {
 
 const ALL_ROOM_QUERY = {
     "GET": ["rooms_shortname", "rooms_lat", "rooms_lon", "rooms_number", "rooms_href"]
-}
-
-const boundQuery = (overlay: any) => {
-    switch (overlay.type) {
-        case google.maps.drawing.OverlayType.RECTANGLE:
-            return RECTANGULAR_QUERY(overlay.overlay.bounds);
-        case google.maps.drawing.OverlayType.CIRCLE:
-            return CIRCULAR_QUERY(overlay.overlay);
-        case google.maps.drawing.OverlayType.POLYGON:
-            return POLYGON_QUERY(overlay.overlay);
-        default:
-            return {};
-    }
-}
-
-const RECTANGULAR_QUERY = (bounds: any) => {
-    return {
-        "AND": [
-            { "GT": { "rooms_lat": bounds.f.f } },
-            { "LT": { "rooms_lat": bounds.f.b } },
-            { "LT": { "rooms_lon": bounds.b.f } },
-            { "GT": { "rooms_lon": bounds.b.b } }
-        ]
-    };
-}
-
-const CIRCULAR_QUERY = (overlay: any) => {
-    return {
-        "WITHIN": {
-            "rooms_lat": overlay.center.lat(),
-            "rooms_lon": overlay.center.lng(),
-            "radius": overlay.radius
-        }
-    };
-}
-
-const POLYGON_QUERY = (bounds: any) => {
-    return {};
 }
 
 const createMarker = (room: any) => {
@@ -157,8 +120,6 @@ export class RoomExplorer extends React.Component<RoomExplorerProps, RoomExplore
                 state.overlays.push(overlay);
                 this.setState(state);
                 return;
-            case google.maps.drawing.OverlayType.POLYGON:
-                return;
             default:
                 return;;
         }
@@ -190,7 +151,7 @@ export class RoomExplorer extends React.Component<RoomExplorerProps, RoomExplore
         let query: any = filters.reduce((query: any, filter: RoomFilterProps) => {
             if (!query.AND) query.AND = [];
 
-            let partialQuery = this.buildFilterQuery(filter);
+            let partialQuery = buildRoomQuery(filter, this.state.subfilters);
             if (!!partialQuery) {
                 query.AND.push(partialQuery);
             }
@@ -198,83 +159,6 @@ export class RoomExplorer extends React.Component<RoomExplorerProps, RoomExplore
             return query;
         }, {});
         return query;
-    }
-
-    buildFilterQuery(filter: RoomFilterProps): {} {
-
-        let f = ROOMS_COLUMNS.find(rc => rc.name === filter.field);
-        let column = f.dataset + f.name;
-
-        switch (filter.type) {
-            case RoomFilterType.CHECKBOX:
-                let subfilter = this.state.subfilters[filter.field];
-
-                if (!filter.value || filter.value.length === 0 || filter.all) {
-                    // return all
-                    return null;
-                } else if (!subfilter || !Object.keys(subfilter).length) {
-                    // find all the selected ones
-                    return { 'OR': filter.value.map((val: string) => { return { 'IS': { [column]: val } } } ) };
-                }
-
-                // find all selected sub options
-                let conversionMap = ['number'].
-                    map((dep: string) => ROOMS_COLUMNS.find(rc => rc.name === dep)).
-                    reduce((map: {[field: string]: string}, rc: any) => {
-                        map[rc.name] = rc.dataset + rc.name;
-                        return map;
-                }, {});
-
-                /*
-                'OR': [
-                    { parent: opt0 && 'AND': [
-                        { suboption0: { OR: [suboptions] } }
-                        { suboption1: { OR: [suboptions] } }
-                    ] }
-                ]
-                */
-
-                let subquery: any = {
-                    'OR': filter.value.map((val: string) => {
-                        return {
-                            'AND': [
-                                { 'IS': { [column]: val } },
-                                { 'OR': Object.keys(subfilter[val]).map(opt => {
-                                    return {
-                                        'AND': Object.keys(subfilter[val][opt]).map(dep => {
-                                            if (subfilter[val][opt][dep]) {
-                                                return { 'IS': { [conversionMap[dep]]: opt } };
-                                            } else {
-                                                return null;
-                                            }
-                                        }).filter(dep => !!dep)
-                                    }
-                                }).filter(q => q.AND && q.AND.length)
-                            }]
-                        }
-                    }).filter((q: any) => q.AND && q.AND.length && q.AND[1].OR && q.AND[1].OR.length)
-                }
-
-                if (!subquery.OR.length) {
-                    return null;
-                }
-                return subquery;
-            case RoomFilterType.RANGE:
-                return {
-                    'OR': [{
-                        'AND': [
-                            { 'GT': { [column]: filter.value.min } },
-                            { 'LT': { [column]: filter.value.max } }
-                        ]
-                    }, {
-                        'EQ': { [column]: filter.value.min }
-                    }, {
-                        'EQ': { [column]: filter.value.max }
-                    }]
-                };
-            default:
-                return null;
-        }
     }
 
     onRangeChange = (field: string, component: any, value: any) => {
