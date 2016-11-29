a9862e885c21ea6c70e50bf97f96f2d784b8de21
Victor Pineda Gonzalez <vpineda1996@gmail.com>
2016-11-29 09:01:38 -0800

    start setting up scheduler

--- src/ui/components/Scheduler.tsx
+++ src/ui/components/Scheduler.tsx
@@ -1,5 +1,8 @@
 import * as React from 'react'
 
+import { WhereCourseSelector } from './WhereCourseSelector'
+import { RoomExplorer } from './RoomExplorer'
+
 export interface SchedulerProps {
     queries: {
         rooms: {}
@@ -10,6 +13,9 @@ export interface SchedulerProps {
 
 export class Scheduler extends React.Component<SchedulerProps, {}> {
     render () {
-        return <div />
+        return <div>
+            <WhereCourseSelector onStatusChanged={()=>{}}/>
+            <RoomExplorer dataId='rooms' onNewQuery={() => {}} />;
+        </div>
     }
 }
